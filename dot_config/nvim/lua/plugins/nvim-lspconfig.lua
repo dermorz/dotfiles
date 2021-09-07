378be26ffa1175409958169aa81405f74eac6881
Moritz Bracht <mail@morz.me>
2021-09-07 09:43:57 +0200

    Update dot_config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,20 +1,18 @@
 local lsp = require 'lspconfig'
 
 -- Go
-local custom_attach = function(client)
-    require('compe_nvim_lsp').attach()
-    require('lsp-status').on_attach(client)
-end
-
-local capabilities = require('lsp-status').capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {
+        'documentation',
+        'detail',
+        'additionalTextEdits',
+    }
+}
 
 lsp.gopls.setup{
-    on_attach = custom_attach,
     capabilities = capabilities,
-    init_options = {
-        usePlaceholders = true;
-    }
 }
 
 -- lua
