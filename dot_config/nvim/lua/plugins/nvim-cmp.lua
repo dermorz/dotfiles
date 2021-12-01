f7c9f2e8b136a185dfb56ea6a5baf22904bf9c16
Moritz Bracht <mail@morz.me>
2021-12-01 11:18:39 +0100

    Update dot_config/nvim/lua/plugins/nvim-cmp.lua

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,10 +1,15 @@
 -- cmp
-require 'cmp'.setup {
+local cmp = require'cmp'
+cmp.setup {
     snippet = {
         expand = function(args)
             vim.fn["UltiSnips#Anon"](args.body)
         end,
-    }
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    })
 
 
 }
