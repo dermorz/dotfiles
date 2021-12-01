547b78fdb63a488f277c5346c461408842987605
Moritz Bracht <mail@morz.me>
2021-12-01 12:07:36 +0100

    Update dot_config/nvim/lua/plugins/nvim-cmp.lua

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -47,6 +47,16 @@ cmp.setup {
                 fallback()
             end
         end, {"i", "s"}),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
     },
 }
 
