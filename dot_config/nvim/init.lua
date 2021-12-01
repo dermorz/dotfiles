909174aa4213c34501423684edf263f7234fe9e7
Moritz Bracht <mail@morz.me>
2021-12-01 09:30:01 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,7 +9,7 @@ require('tools')
 -- plugins
 require('plugins.gruvbox-material')
 require('plugins.lualine')
-require('plugins.nvim-compe')
+require('plugins.nvim-cmp')
 require('plugins.nvim-lspconfig')
 require('plugins.nvim-treesitter')
 require('plugins.telescope')
