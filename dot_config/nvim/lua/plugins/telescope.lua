50553603f499ed5bacaee4e5ef2045dd37ba624b
Moritz Bracht <mail@morz.me>
2022-03-03 15:24:31 +0100

    Update dot_config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ local set_keymap = require('common').set_keymap
 
 set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 set_keymap('n', '<leader>fF', '<cmd>Telescope find_files hidden=true<cr>')
-set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+set_keymap('n', '<leader>rg', '<cmd>Telescope live_grep<cr>')
 set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 set_keymap('n', '<leader>fr', '<cmd>Telescope registers<cr>')
