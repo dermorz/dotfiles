f8addc7286ad07d927a9ea7ec6d11ced854ec429
Moritz Bracht <mail@morz.me>
2022-08-11 14:17:17 +0200

    Add dot_config/nvim/lua/usercommands.lua

---
+++ dot_config/nvim/lua/usercommands.lua
@@ -0,0 +1,15 @@
+vim.api.nvim_create_user_command(
+    'SoftWrap',
+    function()
+        vim.o.wrap = true
+        vim.o.linebreak = true
+        vim.o.showbreak = '+++'
+
+        local opts = { noremap = true, silent = false }
+        vim.api.nvim_buf_set_keymap(0, '', 'j', 'gj', opts)
+        vim.api.nvim_buf_set_keymap(0, '', 'k', 'gk', opts)
+        vim.api.nvim_buf_set_keymap(0, '', '0', 'g0', opts)
+        vim.api.nvim_buf_set_keymap(0, '', '$', 'g$', opts)
+    end,
+    {bang = false, desc = 'Activate smart softwrapping of text'}
+)
